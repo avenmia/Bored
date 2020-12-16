2c672e478ebd190b449c11fde38aad8f1ddcc7d6
avenmia <mavend2@gmail.com>
2020-12-15 19:17:01 -0600

    adding initial signalR hub code.

---
+++ src/Bored.GameService/GameServiceAPI/GameServiceHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Bored.GameService.GameServiceAPI
+{
+    public class GameServiceHub : Hub
+    {
+        public Task SendMessage(string user, string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
--- src/Bored.GameService/Startup.cs
+++ src/Bored.GameService/Startup.cs
@@ -1,3 +1,4 @@
+using Bored.GameService.GameServiceAPI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,7 @@ namespace Bored.GameService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +32,7 @@ namespace Bored.GameService
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapHub<GameServiceHub>("/gameservicehub");
             });
         }
     }
