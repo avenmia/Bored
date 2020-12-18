fa4408ab21c435b95122dce868f70ebe428d64ab
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-12-17 19:31:21 -0600

    add game logic abstract class

---
+++ src/Bored.Common/GameLogic.cs
@@ -0,0 +1,14 @@
+﻿namespace Bored.Common
+{
+    public abstract class GameLogic<State, Move>
+    {
+        public State GameState { get; private set; }
+
+        public GameLogic(State GameState)
+        {
+            this.GameState = GameState;
+        }
+
+        public abstract State GetNextState(State previousState, Move move);
+    }
+}
---
+++ src/Bored.Common/IGameLogic.cs
@@ -0,0 +1,4 @@
+﻿namespace Bored.Common
+{
+   
+}
