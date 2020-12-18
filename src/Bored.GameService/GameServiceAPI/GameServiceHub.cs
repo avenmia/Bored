d6304475ed113ab3bcc9af565f61953b2a6132da
avenmia <mavend2@gmail.com>
2020-12-17 21:20:40 -0600

    Renaming RedisContext to GameSessionContext and adding GameSession interface

--- src/Bored.GameService/GameServiceAPI/GameServiceHub.cs
+++ src/Bored.GameService/GameServiceAPI/GameServiceHub.cs
@@ -1,4 +1,5 @@
 ﻿using Bored.GameService.Clients;
+using Bored.GameService.GameSession;
 using Bored.GameService.Models;
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
@@ -9,6 +10,12 @@ namespace Bored.GameService.GameServiceAPI
     {
         public Task SendMessage(GameMessage message)
         {
+            using (var context = new GameSessionContext())
+            {
+                // var gameState = context.GetGameState();
+                // context.AddGameState(gameState);
+                // Clients.All.ReceiveMessage(gameState);
+            }
             return Clients.All.ReceiveMessage(message);
         }
     }
