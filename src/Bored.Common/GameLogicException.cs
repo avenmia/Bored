6b8c2c9bd36f4494dea0a80e717d29dbd103b3bf
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-12-25 15:26:39 -0600

    add game logic exception to common project

---
+++ src/Bored.Common/GameLogicException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bored.Common
+{
+    public class GameLogicException : Exception
+    {
+        public GameLogicException()
+        {
+        }
+
+        public GameLogicException(string message)
+            : base(message)
+        {
+        }
+
+        public GameLogicException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
