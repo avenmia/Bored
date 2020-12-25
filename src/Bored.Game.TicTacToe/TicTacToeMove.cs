152e92026bae1cc36a5e8ad1c6f863b950bc7282
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-12-25 14:15:04 -0600

    fancy c# constructors

--- src/Bored.Game.TicTacToe/TicTacToeMove.cs
+++ src/Bored.Game.TicTacToe/TicTacToeMove.cs
@@ -5,10 +5,6 @@
         public TicTacToePlayer Player;
         public (byte row, byte col) Cell;
 
-        public TicTacToeMove(TicTacToePlayer _player, (byte row, byte col) _cell)
-        {
-            Player = _player;
-            Cell = _cell;
-        }
+        public TicTacToeMove(TicTacToePlayer player, (byte row, byte col) cell) => (Player, Cell) = (player, cell);
     }
 }
