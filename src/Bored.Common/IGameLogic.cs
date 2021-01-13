c8d19e255511b10f45a956f286c85fe98e72fcee
avenmia <mavend2@gmail.com>
2021-01-12 21:54:29 -0600

    StyleCop Common

--- src/Bored.Common/IGameLogic.cs
+++ src/Bored.Common/IGameLogic.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bored.Common
+﻿namespace Bored.Common
 {
+    /// <summary>
+    /// Interface for the game logic.
+    /// </summary>
     public interface IGameLogic
     {
+        /// <summary>
+        /// The MakeMove method will make a move for a certain game.
+        /// </summary>
+        /// <param name="move">The move to make.</param>
+        /// <returns>The updated game state.</returns>
         public IGameState? MakeMove(IGameMove move);
     }
 }
