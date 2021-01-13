c0b3413bebc81b5910a1116e078510991d969568
avenmia <mavend2@gmail.com>
2021-01-12 21:53:29 -0600

    StyleCop TicTacToe

--- src/Bored.Game.TicTacToe/TicTacToeMove.cs
+++ src/Bored.Game.TicTacToe/TicTacToeMove.cs
@@ -1,23 +1,51 @@
-﻿using Bored.Common;
-using System.Text.Json.Serialization;
-
-namespace Bored.Game.TicTacToe
+﻿namespace Bored.Game.TicTacToe
 {
+    using Bored.Common;
+
     public record TicTacToeCell
     {
+        /// <summary>
+        /// Gets the TicTacToe row.
+        /// </summary>
         public int Row { get; }
+
+        /// <summary>
+        /// Gets the TicTacToe column.
+        /// </summary>
         public int Col { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TicTacToeCell"/> class.
+        /// </summary>
+        /// <param name="row">The TicTacToe row.</param>
+        /// <param name="col">The TicTacToe column.</param>
         public TicTacToeCell(int row, int col) => (Row, Col) = (row, col);
     }
+
+    /// <summary>
+    /// The TicTacToeMove class.
+    /// </summary>
     public class TicTacToeMove : IGameMove
     {
-        public TicTacToePlayer Player;
-        public TicTacToeCell Cell;
-
-        public TicTacToeMove(TicTacToePlayer _player, TicTacToeCell _cell)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TicTacToeMove"/> class.
+        /// </summary>
+        /// <param name="player">The TicTacToe player.</param>
+        /// <param name="cell">The TicTacToe cell being marked.</param>
+        public TicTacToeMove(TicTacToePlayer player, TicTacToeCell cell)
         {
-            Player = _player;
-            Cell = _cell;
+            Player = player;
+            Cell = cell;
         }
+
+        /// <summary>
+        /// Gets or sets the TicTacToe player making the move.
+        /// </summary>
+        public TicTacToePlayer Player { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TicTacToe cell that is being marked.
+        /// </summary>
+        public TicTacToeCell Cell { get; set; }
     }
 }
