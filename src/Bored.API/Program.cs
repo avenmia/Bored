c8902fad8da0bd2485cd39e3d235f56ad6f3121f
avenmia <mavend2@gmail.com>
2021-01-14 20:21:54 -0600

    Adding xml comments to Program and Startup classes in Bored.API

--- src/Bored.API/Program.cs
+++ src/Bored.API/Program.cs
@@ -3,13 +3,25 @@ namespace Bored.API
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
 
+    /// <summary>
+    /// The main program class.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// The entry point for the application.
+        /// </summary>
+        /// <param name="args">The program's arguments.</param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Builds the host.
+        /// </summary>
+        /// <param name="args">The arguements.</param>
+        /// <returns>The host builder.</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
