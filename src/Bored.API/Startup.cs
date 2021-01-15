555b3f942f820bf69a533c9ccd6256ad32b39b7c
avenmia <mavend2@gmail.com>
2021-01-14 20:16:45 -0600

    Reorganizing using directives

--- src/Bored.API/Startup.cs
+++ src/Bored.API/Startup.cs
@@ -1,15 +1,11 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Bored.API
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
