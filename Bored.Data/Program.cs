eb610f0ad220e84a3c3b107f1bfdcba3bc63904e
avenmia <mavend2@gmail.com>
2021-01-19 19:14:46 -0600

    Adding a new data project to house code related to entity framework and database interactions

---
+++ Bored.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bored.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
