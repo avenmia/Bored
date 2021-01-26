0eb130e76d12b13eb798062c52b373bc6e93426b
avenmia <mavend2@gmail.com>
2021-01-25 18:24:05 -0600

    Updating dependencies and configuring live server

--- src/WebApp/snowpack.config.js
+++ src/WebApp/snowpack.config.js
@@ -1,14 +1,23 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: "/", static: true },
-    src: { url: "/dist" },
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
-  plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-dotenv", "@snowpack/plugin-typescript"],
-  install: [
-    /* ... */
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
   ],
-  installOptions: {
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
     /* ... */
   },
   devOptions: {
@@ -16,11 +25,7 @@ module.exports = {
   },
   buildOptions: {
     /* ... */
-  },
-  proxy: {
-    /* ... */
-  },
-  alias: {
-    /* ... */
+    sourcemap: true,
+    watch: true
   },
 };
